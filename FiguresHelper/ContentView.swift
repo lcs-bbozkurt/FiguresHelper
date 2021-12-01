d05aa53995fc521a8b985865001553bcc80a013b
Berk Bozkurt <BBozkurt@lcs.on.ca>
2021-12-01 10:04:20 -0500

    figures helper project

--- FiguresHelper/ContentView.swift
+++ FiguresHelper/ContentView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Circle")
+                .font(.title2)
+                .padding()
+            Text("Radius")
+                .font(.title3)
+                .padding()
+            HStack {
+                Spacer()
+                Text("25.0")
+                Spacer()
+            }
+            
+            Slider(value: .constant(50.0),
+                   in: 0.0...100.0,
+                   label: {
+                        Text("Opacity")
+            },
+                   minimumValueLabel: {
+                        Text("0")
+            },
+                   maximumValueLabel: {
+                        Text("100")
+            })
+                .padding()
+            Text("Area")
+            
+        }
+            .navigationTitle("Figures Helper")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        
+        NavigationView {
         ContentView()
     }
+}
 }
